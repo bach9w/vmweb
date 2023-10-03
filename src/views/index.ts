7e5da045bc18cd7781a0d35deebfd0cf6a602cf2
bach9w <78446671+bach9w@users.noreply.github.com>
2023-10-03 21:18:39 +0300

    Update index.ts

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,6 +1,5 @@
 import Hero from "./Hero";
 import About from "./About";
-import Projects from "./Projects";
-import Contact from "./Contact";
 
-export { Hero, About, Projects, Contact };
+
+export { Hero, About};
